5ff1d9d67268f560d2f493154b56756a938d8587
Steven Moyano <stevenmoyano1@gmail.com>
2021-04-27 14:49:35 -0500

    began to add styling with bootstrap and css. styling 40% complete

--- main.js
+++ main.js
@@ -2,33 +2,39 @@ const body = $("body");
 
 body.append(`
     <div id="app-container">
-        <div id="top-section" class="sections">
+        <div class="row">
             <h1 id="title">Get information on the of your choice</h1>
-            <div class="row">
-                <h2 id="enter-info">Input requested information to receive details of charity</h2>
-            </div>
-            <div class="row">
-                <label>Enter your API Key</label>
-                <input type="text" id="api-key-input">
-            </div>
-            <div class="row">
-            <label>Enter charity EIN</label>
-                <input type="text" id="ein-input">
-            </div>
-            <div class="row">
-                <button id="search-button">Search</button>
-            </div>
         </div>
-        <div id="results" class="sections">
-            <h2 id="result-title">Results</h2>
-        </div>
-        <div id="donation" class="sections">
-            <div class="row">
-                <h3>Would you like to donate?</h3>
+        <div id="sections-parent">
+            <div id="top-section" class="sections">
+                <div class="row">
+                    <h2 id="enter-info">Input requested information to receive details of charity</h2>
+                </div>
+                <div class="row" id="api">
+                    <label class="title-inputs">Enter your API Key</label>
+                    <input type="text" id="api-key-input">
+                </div>
+                <div class="row" id="ein">
+                    <label class="title-inputs">Enter charity EIN</label>
+                    <input type="text" id="ein-input">
+                </div>
+                <div class="row">
+                    <button id="search-button" class="btn btn-secondary">Search</button>
+                </div>
             </div>
-            <div class="row">
-                <label>Enter donation amount</label>
-                <input type="text" id="donation-input">
+
+            <div id="results" class="sections">
+                <h2 id="result-title">Results</h2>
+            </div>
+
+            <div id="donation" class="sections">
+                <div class="row">
+                    <h3>Would you like to donate?</h3>
+                </div>
+                <div class="row">
+                    <label class="title-inputs">Enter donation amount</label>
+                    <input type="text" id="donation-input">
+                </div>
             </div>
         </div>
     </div>
