7c8495e765dc77df22b5e0558ca2092b2c4b6c59
Steven Moyano <stevenmoyano1@gmail.com>
2021-04-28 15:06:00 -0500

    continued adding Javascript and began requesting api data. Ran into a bug that wont allow raw data to be retrieved. Will continue debugging tomorrow

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const body = $("body");
 
+//created and appended all html elements to DOM using jquery
 body.append(`
     <div id="app-container">
         <div class="row">
@@ -39,3 +40,25 @@ body.append(`
         </div>
     </div>
 `);
+
+//queried api text input, ein text input, and search button
+const apiInput = $("#api-key-input");
+const einInput = $("#ein-input");
+const searchButton = $("#search-button");
+
+//add click event listener to search button and fetch api raw data
+searchButton.click(() => {
+  const URL = `http://data.orghunter.com/v1/charitybasic?user_key=${apiInput.val()}&ein=${einInput.val()}`;
+  console.log(URL);
+  fetch(URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json; charset=UTF-8" },
+  })
+    .then((rawResponse) => {
+      console.log("Response", rawResponse);
+      return rawResponse.json();
+    })
+    .then((json) => {
+      console.log("Json", json);
+    });
+});
